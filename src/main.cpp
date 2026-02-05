02a62f5db2d010ba6784cd34270707b9551fb483
corvo-007 <karans00720@gmail.com>
2026-02-05 17:49:06 +0530

    temporarily use SDL2 to display framebuffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,56 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
+// #include <glad/glad.h>
+// #include <GLFW/glfw3.h>
+// #include <iostream>
+//
+// #include "engine-r/window/window.h"
 
-#include "engine-r/window/window.h"
+#include <SDL2/SDL.h>
+#include <vector>
+#include <cstdint>
 
 int main() {
-    const EngineR::Window *window = EngineR::Window::create("test", 800, 600);
-    auto *glfwWindow = static_cast<GLFWwindow *>(window->getNativeWindow());
+    constexpr int WIDTH = 800, HEIGHT = 600;
 
-    glfwMakeContextCurrent(glfwWindow);
+    SDL_Init(SDL_INIT_VIDEO);
 
-    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
-        std::cerr << "Failed to initialize GLAD\n";
-        return -1;
+    SDL_Window *window = SDL_CreateWindow(
+        "Engine-R",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WIDTH,
+        HEIGHT,
+        0
+    );
+
+    SDL_Renderer *sdlRenderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+
+    SDL_Texture *fbTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT)
+                running = false;
+        }
+
+        std::vector<std::uint32_t> framebuffer(WIDTH * HEIGHT);
+
+        SDL_UpdateTexture(fbTexture, nullptr, framebuffer.data(), WIDTH * sizeof(uint32_t));
+
+        SDL_RenderClear(sdlRenderer);
+        SDL_RenderCopy(sdlRenderer, fbTexture, nullptr, nullptr);
+        SDL_RenderPresent(sdlRenderer);
     }
 
-    while (!glfwWindowShouldClose(glfwWindow)) {
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+    SDL_DestroyTexture(fbTexture);
+    SDL_DestroyRenderer(sdlRenderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 1.0f, 1.0f);
-        glVertex2f(0.0f,  0.5f);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
-
-        glfwSwapBuffers(glfwWindow);
-        glfwPollEvents();
-    }
-
-    delete window;
     return 0;
 }
