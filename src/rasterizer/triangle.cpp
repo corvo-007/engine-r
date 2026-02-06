f30d523345bc4cce93379e933823090f610531a7
corvo-007 <karans00720@gmail.com>
2026-02-07 04:35:05 +0530

    add back-face culling

--- src/rasterizer/triangle.cpp
+++ src/rasterizer/triangle.cpp
@@ -6,6 +6,9 @@ namespace EngineR {
     void triangle(EngineM::vec2 p1, EngineM::vec2 p2, EngineM::vec2 p3, const std::uint32_t color, Framebuffer &framebuffer) {
         auto [ p_min, p_max ] = get_bounding_box(p1, p2, p3);
         double total_signed_area = signed_triangle_area(p1, p2, p3);
+        if (total_signed_area < 1) {
+            return ;
+        }
 
         for (int y = p_min.y; y <= p_max.y; y++) {
             for (int x = p_min.x; x <= p_max.x; x++) {
